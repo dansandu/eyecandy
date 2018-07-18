ea7af8a81092690e15fccd45dd491c616c4a4f74
dansandu <daniel.sandu7@gmail.com>
2018-07-18 18:33:56 +0300

    Add scaling transformation

--- source/dansandu/eyecandy/math/transformation.hpp
+++ source/dansandu/eyecandy/math/transformation.hpp
@@ -14,6 +14,25 @@ Matrix<T> identity(typename Matrix<T>::size_type n) {
         result(i, i) = multiplicative_identity<T>;
     return result;
 }
+
+template<int index, typename T>
+void scalingWork(Matrix<T>& matrix) {
+    matrix(index, index) = multiplicative_identity<T>;
+}
+
+template<int index, typename T, typename A, typename... AA>
+void scalingWork(Matrix<T>& matrix, A arg, AA... args) {
+    matrix(index, index) = arg;
+    scalingWork<index + 1, T>(matrix, std::forward<AA>(args)...);
+}
+
+template<typename T, typename... AA>
+Matrix<T> scaling(AA... args) {
+    constexpr auto size = sizeof...(AA) + 1;
+    Matrix<T> result(size, size);
+    scalingWork<0, T>(result, std::forward<AA>(args)...);
+    return result;
+}
 }
 }
 }
--- source/dansandu/eyecandy/math/transformation.t.cpp
+++ source/dansandu/eyecandy/math/transformation.t.cpp
@@ -3,12 +3,20 @@
 
 using dansandu::eyecandy::math::identity;
 using dansandu::eyecandy::math::Matrix;
+using dansandu::eyecandy::math::scaling;
 
 TEST_CASE() {
 
     SECTION("identity") {
         auto actual = identity<int>(3);
         Matrix<int> expected = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+        REQUIRE(actual.closeTo(expected, 0));
+    }
+
+    SECTION("scaling") {
+        auto actual = scaling<int>(2, 3);
+        Matrix<int> expected = {{2, 0, 0}, {0, 3, 0}, {0, 0, 1}};
 
         REQUIRE(actual.closeTo(expected, 0));
     }
