15dc0e8fc1314bdf41e578f00aba330509645574
dansandu <daniel.sandu7@gmail.com>
2018-07-18 14:32:48 +0300

    Add clang format

--- source/dansandu/eyecandy/math/matrix.hpp
+++ source/dansandu/eyecandy/math/matrix.hpp
@@ -13,68 +13,63 @@ namespace math {
 template<typename T>
 class Matrix {
 public:
-  using size_type = int;
-  using value_type = T;
-  using iterator = typename std::vector<value_type>::iterator;
-  using const_iterator = typename std::vector<value_type>::const_iterator;
+    using size_type = int;
+    using value_type = T;
+    using iterator = typename std::vector<value_type>::iterator;
+    using const_iterator = typename std::vector<value_type>::const_iterator;
 
-  Matrix() : rows_{0}, columns_{0} {}
+    Matrix() : rows_{0}, columns_{0} {}
 
-  Matrix(const Matrix& input) = default;
+    Matrix(const Matrix& input) = default;
 
-  Matrix(Matrix&& input) noexcept = default;
+    Matrix(Matrix&& input) noexcept = default;
 
-  Matrix(size_type rows, size_type columns,
-         value_type fillValue = addition_identity<value_type>)
-      : rows_{rows}, columns_{columns} {
-    if (rows < 0 || columns < 0 || (rows != 0) != (columns != 0))
-      throw std::runtime_error("matrix size must be greater or equal than zero");
+    Matrix(size_type rows, size_type columns, value_type fillValue = addition_identity<value_type>)
+        : rows_{rows}, columns_{columns} {
+        if (rows < 0 || columns < 0 || (rows != 0) != (columns != 0))
+            throw std::runtime_error("matrix size must be greater or equal than zero");
 
-    data_ = std::vector<value_type>(rows_ * columns_, fillValue);
-  }
-
-  Matrix(std::initializer_list<std::initializer_list<value_type>> input) {
-    if ((rows_ = input.size()) != 0)
-      columns_ = input.begin()->size();
-    else
-      columns_ = 0;
-
-    for (auto row : input) {
-      data_.insert(data_.end(), row.begin(), row.end());
-      if (columns_ != static_cast<size_type>(row.size()))
-        throw std::runtime_error("columns must be the same size");
+        data_ = std::vector<value_type>(rows_ * columns_, fillValue);
     }
-  }
 
-  size_type rows() const noexcept { return rows_; }
+    Matrix(std::initializer_list<std::initializer_list<value_type>> input) {
+        if ((rows_ = input.size()) != 0)
+            columns_ = input.begin()->size();
+        else
+            columns_ = 0;
 
-  size_type columns() const noexcept { return columns_; }
+        for (auto row : input) {
+            data_.insert(data_.end(), row.begin(), row.end());
+            if (columns_ != static_cast<size_type>(row.size()))
+                throw std::runtime_error("columns must be the same size");
+        }
+    }
 
-  value_type& operator()(size_type row, size_type column) {
-    return data_[columns_ * row + column];
-  }
+    size_type rows() const noexcept { return rows_; }
 
-  value_type operator()(const size_type row, const size_type column) const {
-    return data_[columns_ * row + column];
-  }
+    size_type columns() const noexcept { return columns_; }
 
-  Matrix<value_type> &operator=(const Matrix<value_type> &rhs) = default;
+    value_type& operator()(size_type row, size_type column) { return data_[columns_ * row + column]; }
 
-  Matrix<value_type> &operator=(Matrix<value_type> &&rhs) noexcept = default;
+    value_type operator()(const size_type row, const size_type column) const { return data_[columns_ * row + column]; }
 
-  iterator begin() { return data_.begin(); }
+    Matrix<value_type>& operator=(const Matrix<value_type>& rhs) = default;
 
-  iterator end() { return data_.end(); }
+    Matrix<value_type>& operator=(Matrix<value_type>&& rhs) noexcept = default;
 
-  const_iterator cbegin() const { return data_.cbegin(); }
+    iterator begin() { return data_.begin(); }
 
-  const_iterator cend() const { return data_.cend(); }
+    iterator end() { return data_.end(); }
+
+    const_iterator cbegin() const { return data_.cbegin(); }
+
+    const_iterator cend() const { return data_.cend(); }
+
 private:
-  size_type rows_;
-  size_type columns_;
-  std::vector<value_type> data_;
+    size_type rows_;
+    size_type columns_;
+    std::vector<value_type> data_;
 };
-
 }
 }
 }
--- source/dansandu/eyecandy/math/matrix.t.cpp
+++ source/dansandu/eyecandy/math/matrix.t.cpp
@@ -8,39 +8,38 @@ using dansandu::eyecandy::math::Matrix;
 
 TEST_CASE() {
 
-  SECTION("empty matrix construction") {
-    Matrix<double> emptyMatrix;
+    SECTION("empty matrix construction") {
+        Matrix<double> emptyMatrix;
 
-    REQUIRE(emptyMatrix.rows() == 0);
-    REQUIRE(emptyMatrix.columns() == 0);
-  }
+        REQUIRE(emptyMatrix.rows() == 0);
+        REQUIRE(emptyMatrix.columns() == 0);
+    }
 
-  SECTION("construction") {
-    Matrix<double> matrix(2, 3, 7.0);
+    SECTION("construction") {
+        Matrix<double> matrix(2, 3, 7.0);
 
-    REQUIRE(matrix.rows() == 2);
-    REQUIRE(matrix.columns() == 3);
-    REQUIRE(std::all_of(matrix.cbegin(), matrix.cend(), [](auto elem) { return elem == 7.0; }));
-  }
+        REQUIRE(matrix.rows() == 2);
+        REQUIRE(matrix.columns() == 3);
+        REQUIRE(std::all_of(matrix.cbegin(), matrix.cend(), [](auto elem) { return elem == 7.0; }));
+    }
 
-  SECTION("bad construction") {
-    REQUIRE_THROWS_AS(Matrix<double>(0, 4), std::runtime_error);
-    REQUIRE_THROWS_AS(Matrix<double>(3, 0), std::runtime_error);
-    REQUIRE_THROWS_AS(Matrix<double>(-2, 5), std::runtime_error);
-    REQUIRE_THROWS_AS(Matrix<double>(2, -5), std::runtime_error);
-  }
+    SECTION("bad construction") {
+        REQUIRE_THROWS_AS(Matrix<double>(0, 4), std::runtime_error);
+        REQUIRE_THROWS_AS(Matrix<double>(3, 0), std::runtime_error);
+        REQUIRE_THROWS_AS(Matrix<double>(-2, 5), std::runtime_error);
+        REQUIRE_THROWS_AS(Matrix<double>(2, -5), std::runtime_error);
+    }
 
-  SECTION("construction with initializer list") {
-    Matrix<double> nullMatrix{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
+    SECTION("construction with initializer list") {
+        Matrix<double> nullMatrix{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
 
-    REQUIRE(nullMatrix.rows() == 2);
-    REQUIRE(nullMatrix.columns() == 3);
-  }
+        REQUIRE(nullMatrix.rows() == 2);
+        REQUIRE(nullMatrix.columns() == 3);
+    }
 
-  SECTION("assignment to operator") {
-    Matrix<double> original{{1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}};
-    Matrix<double> copy = original;
-    REQUIRE(std::equal(original.begin(), original.end(), copy.begin(), copy.end()));
-  }
-
+    SECTION("assignment to operator") {
+        Matrix<double> original{{1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}};
+        Matrix<double> copy = original;
+        REQUIRE(std::equal(original.begin(), original.end(), copy.begin(), copy.end()));
+    }
 }
