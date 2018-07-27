0d28b4509fa24dc702d7e52bbe37c13c2d38031f
dansandu <daniel.sandu7@gmail.com>
2018-07-27 16:42:44 +0300

    Remove mesh rasterization

--- source/dansandu/eyecandy/raster/fragment.hpp
+++ source/dansandu/eyecandy/raster/fragment.hpp
@@ -8,18 +8,6 @@ namespace eyecandy {
 namespace raster {
 
 void drawLine(Image& image, int x0, int y0, int x1, int y1, Color color);
-
-template<typename T>
-void drawMesh(Image& image, const geometry::Mesh<T>& mesh, Color color) {
-    for (const auto& triangle : mesh.triangles) {
-        drawLine(image, mesh.vertices(0, triangle.v0), mesh.vertices(1, triangle.v0), mesh.vertices(0, triangle.v1),
-                 mesh.vertices(1, triangle.v1), color);
-        drawLine(image, mesh.vertices(0, triangle.v0), mesh.vertices(1, triangle.v0), mesh.vertices(0, triangle.v2),
-                 mesh.vertices(1, triangle.v2), color);
-        drawLine(image, mesh.vertices(0, triangle.v1), mesh.vertices(1, triangle.v1), mesh.vertices(0, triangle.v2),
-                 mesh.vertices(1, triangle.v2), color);
-    }
-}
 }
 }
 }
