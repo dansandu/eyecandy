d7f44e60cd40fc6641e6308378a6d49499c56012
dansandu <daniel.sandu7@gmail.com>
2018-08-21 09:52:41 +0300

    Add movement to animation

--- source/dansandu/eyecandy/main.cpp
+++ source/dansandu/eyecandy/main.cpp
@@ -33,24 +33,49 @@ int main() {
     sf::Sprite sprite(texture);
     Image pixels{resolution.first, resolution.second};
 
-    Matrix<double> eye{0.0, 0.0, 0.0}, target{0.0, 0.0, -300.0}, up{0.0, 1.0, 0.0};
+    Matrix<double> eye{0.0, 0.0, 0.0}, target{0.0, 0.0, -300.0}, up{0.0, 1.0, 0.0}, spherePosition{0.0, 0.0, -300.0};
 
     auto mySphere = sphere(150.0, 10, 10), sphereCopy = mySphere;
     auto pipeline = viewport<double>(resolution.first, resolution.second) * perspective(1.0, 2000.0, 1.92, 1.5) *
-                    lookAt(eye, target, up) * translation(0.0, 0.0, -300.0);
+                    lookAt(eye, target, up);
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::KeyReleased) {
+                switch (event.key.code) {
+                case sf::Keyboard::F:
+                    spherePosition(0) += 10.0;
+                    break;
+                case sf::Keyboard::S:
+                    spherePosition(0) -= 10.0;
+                    break;
+                case sf::Keyboard::E:
+                    spherePosition(1) += 10.0;
+                    break;
+                case sf::Keyboard::D:
+                    spherePosition(1) -= 10.0;
+                    break;
+                case sf::Keyboard::W:
+                    spherePosition(2) += 10.0;
+                    break;
+                case sf::Keyboard::R:
+                    spherePosition(2) -= 10.0;
+                    break;
+                default:
+                    break;
+                }
+            }
         }
 
         auto now = std::chrono::steady_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
 
-        sphereCopy.vertices =
-            pipeline * rotationByY(elapsed.count() % 5000 / 5000.0 * 2.0 * pi<double>) * mySphere.vertices;
+        sphereCopy.vertices = pipeline * translation(spherePosition(0), spherePosition(1), spherePosition(2)) *
+                              rotationByY(elapsed.count() % 5000 / 5000.0 * 2.0 * pi<double>) * mySphere.vertices;
         sphereCopy.vertices.dehomogenize();
         pixels.clear();
         drawMeshWireframe(pixels, sphereCopy, Colors::magenta);
