7542c8f5ca5567ff6e54622fd0ef144653baf698
dansandu <daniel.sandu7@gmail.com>
2018-08-26 02:17:44 +0300

    Add string formatting

--- source/dansandu/eyecandy/utility/string.t.cpp
+++ source/dansandu/eyecandy/utility/string.t.cpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 
+using dansandu::eyecandy::utility::format;
 using dansandu::eyecandy::utility::join;
 
 TEST_CASE("string utility") {
@@ -26,5 +27,18 @@ TEST_CASE("string utility") {
             std::vector<int> integers;
             REQUIRE(join(integers, "") == "");
         }
+    }
+
+    SECTION("format") {
+        SECTION("too few arguments") { REQUIRE_THROWS_AS(format("# + # = #", 2, 3), std::invalid_argument); }
+
+        SECTION("too many arguments") {
+            REQUIRE_THROWS_AS(format("# + # = #", 2, 3, 5, 0), std::invalid_argument);
+            REQUIRE_THROWS_AS(format("no formatting", 1), std::invalid_argument);
+        }
+
+        SECTION("no arguments") { REQUIRE(format("no formatting") == "no formatting"); }
+
+        REQUIRE(format("The # jumped the # twice", "cat", "fence") == "The cat jumped the fence twice");
     }
 }
