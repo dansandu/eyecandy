7ee8d065a1d156fd913ec459e1085bc431f12f76
dansandu <daniel.sandu7@gmail.com>
2018-08-26 02:22:16 +0300

    Fix parametric formula

--- source/dansandu/eyecandy/geometry/clipping.hpp
+++ source/dansandu/eyecandy/geometry/clipping.hpp
@@ -3,6 +3,8 @@
 #include "dansandu/eyecandy/geometry/mesh.hpp"
 #include "dansandu/eyecandy/math/matrix.hpp"
 
+#include <iostream>
+
 namespace dansandu {
 namespace eyecandy {
 namespace geometry {
@@ -15,30 +17,27 @@ auto clipCode(const math::Matrix<T>& vertex) {
 
 template<typename T>
 Mesh<T> clip(const Mesh<T>& mesh) {
-    using size_type = typename std::vector<T>::size_type;
     using dansandu::eyecandy::math::Matrix;
-
+    constexpr auto w = 3;
     std::vector<T> vertices;
     std::vector<int> triangles;
     for (auto triangle = 0; triangle < mesh.triangleCount(); ++triangle) {
-        std::vector<Matrix<T>> polygon = {mesh.vertex(triangle, 0), mesh.vertex(triangle, 1), mesh.vertex(triangle, 2)};
-        std::vector<Matrix<T>> newPolygon;
+        std::vector<Matrix<T>> polygon = {mesh.vertex(triangle, 0), mesh.vertex(triangle, 1), mesh.vertex(triangle, 2)},
+                               clippedPolygon;
         for (auto side = -1.0; side < 2.0; side += 2.0)
             for (auto axis = 0; axis < 3; ++axis) {
-                for (size_type a = 0, b = 1; a < polygon.size(); ++a, b = (a + 1) % polygon.size()) {
-                    if (-polygon[a](3) < -side * polygon[a](axis))
-                        newPolygon.push_back(polygon[a]);
-
-                    auto t = (side * polygon[a](3) * polygon[b](3) - polygon[b](3) * polygon[a](axis)) /
-                             (polygon[a](3) * polygon[b](axis) - polygon[b](3) * polygon[a](axis));
-
+                for (auto a = 0, b = 1; a < static_cast<int>(polygon.size()); b = (++a + 1) % polygon.size()) {
+                    if (polygon[a](w) > side * polygon[a](axis))
+                        clippedPolygon.push_back(polygon[a]);
+                    auto t = (side * polygon[a](w) - polygon[a](axis)) /
+                             (polygon[b](axis) - polygon[a](axis) - side * (polygon[b](w) - polygon[a](w)));
                     if (0.0 < t && t < 1.0)
-                        newPolygon.push_back(polygon[a] + (polygon[b] - polygon[a]) * t);
+                        clippedPolygon.push_back(polygon[a] + (polygon[b] - polygon[a]) * t);
                 }
-                polygon = std::move(newPolygon);
+                polygon = std::move(clippedPolygon);
             }
 
-        auto offset = static_cast<int>(vertices.size());
+        auto offset = static_cast<int>(vertices.size()) / 4;
         for (auto i = 0; i < static_cast<int>(polygon.size()); ++i) {
             vertices.insert(vertices.end(), polygon[i].begin(), polygon[i].end());
             if (i > 1)
