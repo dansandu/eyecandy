69e640abf498ddc36693b1f1e6e79cb8d45d280a
dansandu <daniel.sandu7@gmail.com>
2018-10-08 16:24:33 +0300

    Revamp matrix class

---
+++ source/dansandu/eyecandy/math/heap_matrix.hpp
@@ -0,0 +1,202 @@
+#pragma once
+
+#include "dansandu/eyecandy/math/base_matrix.hpp"
+#include "dansandu/eyecandy/math/numeric_traits.hpp"
+#include "dansandu/eyecandy/utility/exception.hpp"
+
+#include <type_traits>
+#include <vector>
+
+namespace dansandu {
+namespace eyecandy {
+namespace math {
+
+template<typename T, size_type staticRows, size_type staticColumns>
+class Matrix<T, staticRows, staticColumns, StorageStrategy::heap> {
+public:
+    using value_type = T;
+
+private:
+    static_assert(staticRows > 0 || staticRows == dynamic, "matrix rows must be positive or dynamic");
+    static_assert(staticColumns > 0 || staticColumns == dynamic, "matrix columns must be positive or dynamic");
+
+    constexpr static size_type staticSize_{staticRows * staticColumns};
+    constexpr static auto isStaticVector_{staticRows == 1 || staticColumns == 1};
+
+public:
+    template<typename U = value_type, typename = std::enable_if_t<staticRows == dynamic || staticColumns == dynamic, U>>
+    Matrix() : rows_{staticRows == dynamic ? 0 : staticRows}, columns_{staticColumns == dynamic ? 0 : staticColumns} {}
+
+    template<typename U = value_type, typename = std::enable_if_t<staticRows != dynamic && staticColumns != dynamic, U>>
+    explicit Matrix(value_type fillValue = dansandu::eyecandy::math::additive_identity<value_type>)
+        : rows_{staticRows}, columns_{staticColumns}, data_(rows_ * columns_, fillValue) {}
+
+    template<typename U = value_type, size_type length,
+             typename = std::enable_if_t<isStaticVector_ && staticSize_ == length && length >= 1, U>>
+    explicit Matrix(const value_type (&array)[length])
+        : rows_{staticRows}, columns_{staticColumns}, data_(std::begin(array), std::end(array)) {}
+
+    template<typename U = value_type, size_type rowCount, size_type columnCount,
+             typename = std::enable_if_t<
+                 staticRows == rowCount && staticColumns == columnCount && rowCount >= 1 && columnCount >= 1, U>>
+    explicit Matrix(const value_type (&array)[rowCount][columnCount]) : rows_{rowCount}, columns_{columnCount} {
+        data_.reserve(rowCount * columnCount);
+        for (auto row = 0; row < rowCount; ++row)
+            for (auto column = 0; column < columnCount; ++column)
+                data_.push_back(array[row][column]);
+    }
+
+    template<typename U = value_type, typename = std::enable_if_t<staticRows == dynamic || staticColumns == dynamic, U>>
+    Matrix(size_type rowCount, size_type columnCount, std::vector<value_type> buffer)
+        : rows_{rowCount}, columns_{columnCount}, data_{std::move(buffer)} {
+        if (rows_ < 0 || columns_ < 0)
+            THROW(std::invalid_argument,
+                  "invalid dimensions #x# provided in matrix constructor -- rows and columns must be greater than or "
+                  "equal to zero",
+                  rows_, columns_);
+
+        if ((staticRows != rows_ && staticRows != dynamic) || (staticColumns != columns_ && staticColumns != dynamic))
+            THROW(std::invalid_argument, "static dimensions #x# do not match dynamic dimensions #x#", staticRows,
+                  staticColumns, rows_, columns_);
+
+        if (rows_ * columns_ != static_cast<size_type>(data_.size()))
+            THROW(std::invalid_argument, "buffer size # doesn't match matrix size #", data_.size(), rows_ * columns_);
+    }
+
+    Matrix(size_type rowCount, size_type columnCount,
+           value_type fillValue = dansandu::eyecandy::math::additive_identity<value_type>)
+        : rows_{rowCount}, columns_{columnCount} {
+        if (rows_ < 0 || columns_ < 0)
+            THROW(std::invalid_argument,
+                  "invalid dimensions #x# provided in matrix constructor -- rows and columns must be greater than or "
+                  "equal to zero",
+                  rows_, columns_);
+
+        if ((staticRows != rows_ && staticRows != dynamic) || (staticColumns != columns_ && staticColumns != dynamic))
+            THROW(std::invalid_argument, "static dimensions #x# do not match dynamic dimensions #x#", staticRows,
+                  staticColumns, rows_, columns_);
+
+        data_ = std::vector<value_type>(rows_ * columns_, fillValue);
+    }
+
+    Matrix(const Matrix&) = default;
+
+    Matrix(Matrix&&) noexcept = default;
+
+    Matrix& operator=(const Matrix&) = default;
+
+    Matrix& operator=(Matrix&&) noexcept = default;
+
+    Matrix& operator+=(const Matrix& other) {
+        if (rows_ != other.rows_ || columns_ != other.columns_)
+            THROW(std::invalid_argument,
+                  "cannot add right hand side #x# matrix to left hand side #x# matrix -- matrix dimensions must match",
+                  other.rows_, other.columns_, rows_, columns_);
+
+        std::transform(begin(), end(), other.begin(), begin(), std::plus<T>{});
+        return *this;
+    }
+
+    Matrix& operator-=(const Matrix& other) {
+        if (rows_ != other.rows_ || columns_ != other.columns_)
+            THROW(std::invalid_argument,
+                  "cannot subtract right hand side #x# matrix from left hand side #x# matrix -- matrix dimensions must "
+                  "match",
+                  other.rows_, other.columns_, rows_, columns_);
+
+        std::transform(begin(), end(), other.begin(), begin(), std::minus<T>{});
+        return *this;
+    }
+
+    Matrix& operator*=(T scalar) {
+        for (auto& element : data_)
+            element *= scalar;
+        return *this;
+    }
+
+    auto swap(Matrix& other) noexcept {
+        using std::swap;
+        swap(rows_, other.rows_);
+        swap(columns_, other.columns_);
+        swap(data_, other.data_);
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 1, U>>
+    auto& x() {
+        return data_[0];
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 2, U>>
+    auto& y() {
+        return data_[1];
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 3, U>>
+    auto& z() {
+        return data_[2];
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 4, U>>
+    auto& w() {
+        return data_[3];
+    }
+
+    auto& operator()(size_type row, size_type column) { return data_[index(row, column)]; }
+
+    auto begin() { return std::begin(data_); }
+
+    auto end() { return std::end(data_); }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 1, U>>
+    auto& x() const {
+        return data_[0];
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 2, U>>
+    auto& y() const {
+        return data_[1];
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 3, U>>
+    auto& z() const {
+        return data_[2];
+    }
+
+    template<typename U = size_type, typename = std::enable_if_t<isStaticVector_ && staticSize_ >= 4, U>>
+    auto& w() const {
+        return data_[3];
+    }
+
+    auto operator()(size_type row, size_type column) const { return data_[index(row, column)]; }
+
+    auto begin() const { return std::begin(data_); }
+
+    auto end() const { return std::end(data_); }
+
+    auto rows() const { return rows_; }
+
+    auto columns() const { return columns_; }
+
+    auto closeTo(const Matrix& other, T epsilon) {
+        return rows_ == other.rows_ && columns_ == other.columns_ &&
+               std::equal(begin(), end(), other.begin(), other.end(),
+                          [epsilon](auto a, auto b) { return dansandu::eyecandy::math::closeTo(a, b, epsilon); });
+    }
+
+private:
+    auto index(size_type row, size_type column) const {
+        if (row < 0 || row >= rows_ || column < 0 || column >= columns_)
+            THROW(std::out_of_range,
+                  "cannot index the #th row and #th column in a #x# matrix -- indices are out of bounds", row, column,
+                  rows_, columns_);
+
+        return row * columns_ + column;
+    }
+
+    size_type rows_;
+    size_type columns_;
+    std::vector<value_type> data_;
+};
+}
+}
+}
--- source/dansandu/eyecandy/math/matrix.hpp
+++ source/dansandu/eyecandy/math/matrix.hpp
@@ -1,311 +1,158 @@
 #pragma once
 
+#include "dansandu/eyecandy/math/base_matrix.hpp"
+#include "dansandu/eyecandy/math/heap_matrix.hpp"
+#include "dansandu/eyecandy/math/matrix_slice.hpp"
 #include "dansandu/eyecandy/math/numeric_traits.hpp"
+#include "dansandu/eyecandy/math/stack_matrix.hpp"
 #include "dansandu/eyecandy/utility/exception.hpp"
 
 #include <algorithm>
+#include <cmath>
+#include <functional>
 #include <iomanip>
 #include <sstream>
-#include <stdexcept>
-#include <vector>
+#include <type_traits>
 
 namespace dansandu {
 namespace eyecandy {
 namespace math {
 
-template<typename T>
-class Matrix {
-public:
-    using size_type = int;
-    using value_type = T;
-
-    Matrix() : rows_{0}, columns_{0} {}
-
-    Matrix(const Matrix& rhs) = default;
-
-    Matrix(Matrix&& rhs) noexcept : rows_{rhs.rows_}, columns_{rhs.columns_}, data_{std::move(rhs.data_)} {
-        rhs.rows_ = rhs.columns_ = 0;
-    }
-
-    Matrix(size_type rowCount, size_type columnCount, value_type fillValue = additive_identity<value_type>)
-        : rows_{rowCount}, columns_{columnCount} {
-        if (rows_ < 0 || columns_ < 0)
-            THROW(std::invalid_argument,
-                  "invalid dimensions #x# provided in matrix constructor -- rows and columns must be greater than or "
-                  "equal to zero",
-                  rows_, columns_);
-
-        data_ = std::vector<T>(rows_ * columns_, fillValue);
-    }
-
-    Matrix(std::initializer_list<value_type> column)
-        : rows_{static_cast<size_type>(column.size())}, columns_{1}, data_(column) {}
-
-    Matrix(std::initializer_list<std::initializer_list<value_type>> list)
-        : rows_{static_cast<size_type>(list.size())},
-          columns_{rows_ ? static_cast<size_type>(list.begin()->size()) : 0} {
-        for (auto row : list) {
-            if (columns_ != static_cast<size_type>(row.size()))
-                THROW(std::invalid_argument,
-                      "conflicting sizes for rows provided in matrix constructor (# was expected but # was provided) "
-                      "-- rows must have the same size",
-                      columns_, row.size());
-            data_.insert(data_.end(), row.begin(), row.end());
-        }
-    }
-
-    Matrix(size_type rowCount, size_type columnCount, std::vector<value_type> buffer)
-        : rows_{rowCount}, columns_{columnCount}, data_{std::move(buffer)} {
-        if (rows_ < 0 || columns_ < 0)
-            THROW(std::invalid_argument,
-                  "invalid dimensions #x# provided in matrix constructor -- rows and columns must be greater than or "
-                  "equal to zero",
-                  rowCount, columnCount);
-
-        if (rows_ * columns_ != static_cast<int>(data_.size()))
-            THROW(std::invalid_argument,
-                  "invalid dimensions #x# provided in matrix constructor -- the buffer contains # elements but # were "
-                  "expected",
-                  rows_, columns_, data_.size(), rows_ * columns_);
-
-        if (rows_ == 0 || columns_ == 0) {
-            rows_ = columns_ = 0;
-            data_.clear();
-        }
-    }
-
-    Matrix<T>& operator=(const Matrix& rhs) = default;
-
-    auto& operator=(Matrix&& rhs) noexcept {
-        if (this != &rhs) {
-            rows_ = rhs.rows_;
-            columns_ = rhs.columns_;
-            data_ = std::move(rhs.data_);
-            rhs.rows_ = rhs.columns_ = 0;
-        }
-        return *this;
-    }
-
-    auto& operator*=(const Matrix& rhs) { return *this = *this * rhs; }
-
-    auto& operator*=(value_type scalar) {
-        for (auto& element : data_)
-            element *= scalar;
-        return *this;
-    }
-
-    auto& operator-=(const Matrix& other) {
-        if (rows_ != other.rows_ || columns_ != other.columns_)
-            THROW(std::invalid_argument,
-                  "cannot subtract right hand side #x# matrix from left hand side #x# matrix -- matrix dimensions must "
-                  "match",
-                  other.rows(), other.columns(), rows(), columns());
-
-        std::transform(data_.begin(), data_.end(), other.data_.begin(), data_.begin(),
-                       [](auto a, auto b) { return a - b; });
-        return *this;
-    }
-
-    auto& operator+=(const Matrix& other) {
-        if (rows_ != other.rows_ || columns_ != other.columns_)
-            THROW(std::invalid_argument,
-                  "cannot add right hand side #x# matrix to left hand side #x# matrix -- matrix dimensions must "
-                  "match",
-                  other.rows(), other.columns(), rows(), columns());
-
-        std::transform(data_.begin(), data_.end(), other.data_.begin(), data_.begin(),
-                       [](auto a, auto b) { return a + b; });
-        return *this;
-    }
-
-    auto& operator()(size_type n) { return data_[index(n)]; }
-
-    auto& operator()(size_type row, size_type column) { return data_[index(row, column)]; }
-
-    auto swap(Matrix& other) noexcept {
-        using std::swap;
-        swap(rows_, other.rows_);
-        swap(columns_, other.columns_);
-        swap(data_, other.data_);
-    }
-
-    auto operator()(size_type n) const { return data_[index(n)]; }
-
-    auto operator()(const size_type row, const size_type column) const { return data_[index(row, column)]; }
-
-    auto rows() const { return rows_; }
-
-    auto columns() const { return columns_; }
-
-    auto row(size_type n, std::pair<int, int> slice) const {
-        auto start = data_.begin() + index(n, 0) + slice.first;
-        auto end = start + slice.second;
-        return Matrix<T>(1, end - start, std::vector<value_type>(start, end));
-    }
-
-    auto row(size_type n) const { return row(n, {0, columns()}); }
-
-    auto closeTo(const Matrix& rhs, value_type epsilon) const {
-        return rows_ == rhs.rows_ && columns_ == rhs.columns_ &&
-               std::equal(data_.begin(), data_.end(), rhs.data_.begin(), rhs.data_.end(),
-                          [epsilon](auto a, auto b) { return dansandu::eyecandy::math::closeTo(a, b, epsilon); });
-    }
-
-    auto toString() const {
-        std::stringstream ss;
-        ss << std::fixed << std::setprecision(2) << *this;
-        return ss.str();
-    }
-
-    auto begin() const { return data_.begin(); }
-
-    auto end() const { return data_.end(); }
-
-private:
-    auto index(size_type n) const {
-        if (rows_ != 1 && columns_ != 1)
-            THROW(std::runtime_error,
-                  "cannot single index the #th element in a #x# matrix -- matrix must be row or column vector", n,
-                  rows_, columns_);
-
-        if (n < 0 || n >= static_cast<size_type>(data_.size()))
-            THROW(std::out_of_range, "cannot index the #th element in a #-lengthed vector -- index out of bounds", n,
-                  data_.size());
-
-        return n;
-    }
-
-    auto index(size_type row, size_type column) const {
-        if (row < 0 || row >= rows_ || column < 0 || column >= columns_)
-            THROW(std::out_of_range,
-                  "cannot index the #th row and #th column in a #x# matrix -- indices are out of bounds", row, column,
-                  rows_, columns_);
-
-        return row * columns_ + column;
-    }
-
-    size_type rows_;
-    size_type columns_;
-    std::vector<value_type> data_;
-};
-
-template<typename T>
-auto operator*(const Matrix<T>& lhs, const Matrix<T>& rhs) {
-    if (lhs.columns() != rhs.rows())
-        THROW(std::invalid_argument,
-              "cannot multiply left hand side #x# matrix with right hand side #x# matrix -- left hand side columns "
-              "must match right hand side rows",
-              lhs.rows(), lhs.columns(), rhs.rows(), rhs.columns());
-
-    Matrix<T> result(lhs.rows(), rhs.columns());
-    for (auto i = 0; i < lhs.rows(); ++i)
-        for (auto k = 0; k < rhs.columns(); ++k)
-            for (auto j = 0; j < lhs.columns(); ++j)
-                result(i, k) += lhs(i, j) * rhs(j, k);
-    return result;
+template<typename T, size_type M, size_type N>
+auto closeTo(Matrix<T, M, N>& a, Matrix<T, M, N>& b, T epsilon) {
+    return a.closeTo(b, epsilon);
 }
 
-template<typename T>
-auto operator-(const Matrix<T>& lhs, const Matrix<T>& rhs) {
-    auto result = lhs;
-    return result -= rhs;
+template<typename T, size_type M, size_type N>
+auto swap(Matrix<T, M, N>& a, Matrix<T, M, N>& b) {
+    a.swap(b);
 }
 
-template<typename T>
-auto operator+(const Matrix<T>& lhs, const Matrix<T>& rhs) {
-    auto result = lhs;
-    return result += rhs;
+template<typename T, size_type M, size_type N>
+auto operator+(const Matrix<T, M, N>& a, const Matrix<T, M, N> b) {
+    auto result = a;
+    return result += b;
 }
 
-template<typename T>
-auto& operator<<(std::ostream& os, const Matrix<T>& matrix) {
-    for (auto i = 0; i < matrix.rows(); ++i) {
-        auto j = 0;
-        while (j + 1 < matrix.columns())
-            os << matrix(i, j++) << ", ";
-        os << matrix(i, j) << std::endl;
-    }
-    return os;
+template<typename T, size_type M, size_type N>
+auto operator-(const Matrix<T, M, N>& a, const Matrix<T, M, N> b) {
+    auto result = a;
+    return result -= b;
 }
 
-template<typename T>
-auto swap(Matrix<T>& lhs, Matrix<T>& rhs) {
-    lhs.swap(rhs);
-}
-
-template<typename T>
-auto operator*(const Matrix<T>& matrix, T scalar) {
-    Matrix<T> result = matrix;
+template<typename T, size_type M, size_type N>
+auto operator*(const Matrix<T, M, N>& matrix, T scalar) {
+    auto result = matrix;
     return result *= scalar;
 }
 
-template<typename T>
-auto operator*(T scalar, const Matrix<T>& matrix) {
-    Matrix<T> result = matrix;
+template<typename T, size_type M, size_type N>
+auto operator*(T scalar, const Matrix<T, M, N>& matrix) {
+    auto result = matrix;
     return result *= scalar;
 }
 
-template<typename T>
-auto operator-(const Matrix<T>& matrix) {
-    return matrix * -multiplicative_identity<T>;
+template<typename T, size_type M, size_type N, typename = std::enable_if_t<std::is_integral_v<T>>>
+auto operator==(const Matrix<T, M, N>& a, const Matrix<T, M, N> b) {
+    return a.rows() == b.rows() && a.columns() && b.columns() && std::equal(std::begin(a), std::end(a), std::begin(b));
 }
 
-template<typename T>
-auto magnitude(const Matrix<T>& matrix) {
-    if (matrix.rows() != 1 && matrix.columns() != 1)
-        THROW(std::invalid_argument, "cannot take the magnitude of a #x# matrix -- matrix must be row or column vector",
-              matrix.rows(), matrix.columns());
-
-    auto result = additive_identity<T>;
-    for (auto element : matrix)
-        result += element * element;
-    return std::sqrt(result);
+template<typename T, size_type M, size_type N, typename = std::enable_if_t<std::is_integral_v<T>>>
+auto operator!=(const Matrix<T, M, N>& a, const Matrix<T, M, N> b) {
+    return !(a == b);
 }
 
-template<typename T>
-auto normalized(const Matrix<T>& matrix) {
-    return (multiplicative_identity<T> / magnitude(matrix)) * matrix;
+template<typename T, size_type M, size_type N, size_type K>
+auto operator*(const Matrix<T, M, N>& a, const Matrix<T, N, K>& b) {
+    Matrix<T, M, K> result{a.rows(), b.columns()};
+    for (auto i = 0; i < a.rows(); ++i)
+        for (auto k = 0; k < b.columns(); ++k)
+            for (auto j = 0; j < a.columns(); ++j)
+                result(i, k) += a(i, j) * b(j, k);
+
+    if constexpr (M == 1 && K == 1)
+        return result.x();
+    else
+        return result;
 }
 
-template<typename T>
-auto crossProduct(const Matrix<T>& lhs, const Matrix<T>& rhs) {
-    if (lhs.rows() * lhs.columns() != 3 || rhs.rows() * rhs.columns() != 3)
-        THROW(std::invalid_argument,
-              "cannot take the cross product of two #x# and #x# matrices -- matrices must be row or column vectors of "
-              "length 3",
-              lhs.rows(), lhs.columns(), rhs.rows(), rhs.columns());
-
-    // clang-format off
-    return Matrix<T>{lhs(1) * rhs(2) - rhs(1) * lhs(2),
-                     rhs(0) * lhs(2) - lhs(0) * rhs(2),
-                     lhs(0) * rhs(1) - lhs(1) * rhs(0)};
-    // clang-format on
+template<typename T, size_type M, size_type N>
+auto operator-(const Matrix<T, M, N>& matrix) {
+    constexpr auto _1 = dansandu::eyecandy::math::multiplicative_identity<T>;
+    return -_1 * matrix;
 }
 
-template<typename T>
-auto dotProduct(const Matrix<T>& lhs, const Matrix<T>& rhs) {
-    if ((lhs.rows() != 1 && lhs.columns() != 1) || (rhs.rows() != 1 && rhs.columns() != 1) ||
-        lhs.rows() * lhs.columns() != rhs.rows() * rhs.columns())
-        THROW(std::invalid_argument,
-              "cannot take the dot product of two #x# and #x# matrices -- matrices must be row or column vectors of "
-              "the same length",
-              lhs.rows(), lhs.columns(), rhs.rows(), rhs.columns());
-
-    auto product = additive_identity<T>;
-    for (auto lhsItr = lhs.begin(), rhsItr = rhs.begin(); lhsItr != lhs.end(); ++lhsItr, ++rhsItr)
-        product += *lhsItr * *rhsItr;
-
-    return product;
+template<typename T, size_type M, size_type N, typename = std::enable_if_t<M == 1 || N == 1, T>>
+auto toString(const Matrix<T, M, N>& vector, size_type decimalPoints = 2) {
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(decimalPoints) << "(";
+    auto i = 0;
+    for (; i + 1 < std::max(M, N); ++i)
+        ss << vector(i) << ", ";
+    ss << vector(i) << ")";
+    return ss.str();
 }
 
-template<typename T>
-auto transpose(const Matrix<T>& matrix) {
-    Matrix<T> result(matrix.columns(), matrix.rows());
-    for (auto i = 0; i < result.rows(); i++)
-        for (auto j = 0; j < result.columns(); j++)
-            result(i, j) = matrix(j, i);
+template<typename T, size_type M, size_type N, typename = std::enable_if_t<M == 1 || N == 1, T>>
+auto normalized(const Matrix<T, M, N>& vector) {
+    auto _1 = dansandu::eyecandy::math::multiplicative_identity<T>;
+    return (_1 / magnitude(vector)) * vector;
+}
+
+template<typename T, size_type M, size_type N, size_type K, size_type L,
+         typename = std::enable_if_t<(M == 1 || N == 1) && (K == 1 || L == 1) && M * N == K * L, T>>
+auto dotProduct(const Matrix<T, M, N>& a, const Matrix<T, K, L>& b) {
+    auto sum = dansandu::eyecandy::math::additive_identity<T>;
+    for (auto left = a.begin(), right = b.begin(); left != a.end(); ++left, ++right)
+        sum += *left * *right;
+    return sum;
+}
+
+template<typename T, size_type M, size_type N, typename = std::enable_if_t<M == 1 || N == 1, T>>
+auto magnitude(const Matrix<T, M, N>& vector) {
+    return static_cast<T>(std::sqrt(dotProduct(vector, vector)));
+}
+
+template<typename T, size_type M, size_type N, typename = std::enable_if_t<(M == 1 || N == 1) && M * N == 3, T>>
+auto crossProduct(const Matrix<T, M, N>& a, const Matrix<T, M, N>& b) {
+    auto x = a.y() * b.z() - b.y() * a.z();
+    auto y = b.x() * a.z() - a.x() * b.z();
+    auto z = a.x() * b.y() - b.x() * a.y();
+    return Matrix<T, M, N>{{x, y, z}};
+}
+
+template<typename U, typename T, size_type M, size_type N>
+auto matrix_cast(const Matrix<T, M, N>& matrix) {
+    Matrix<U, M, N> result;
+    std::transform(std::begin(matrix), std::end(matrix), std::begin(result),
+                   [](auto element) { return static_cast<U>(element); });
     return result;
 }
+
+template<typename T, size_type M>
+auto dehomogenize(Matrix<T, M, 4>& matrix) {
+    for (auto i = 0; i < matrix.rows(); ++i)
+        for (auto j = 0; j < matrix.columns(); ++j)
+            matrix(i, j) /= matrix(i, matrix.columns() - 1);
+}
+
+template<typename T>
+using RowVector2 = Matrix<T, 1, 2>;
+
+template<typename T>
+using RowVector3 = Matrix<T, 1, 3>;
+
+template<typename T>
+using RowVector4 = Matrix<T, 1, 4>;
+
+template<typename T>
+using ColumnVector3 = Matrix<T, 3, 1>;
+
+template<typename T>
+using ColumnVector4 = Matrix<T, 4, 1>;
+
+template<typename T>
+using Matrix4 = Matrix<T, 4, 4>;
 }
 }
 }
