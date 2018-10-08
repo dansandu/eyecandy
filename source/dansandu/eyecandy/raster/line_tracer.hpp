2575075b4c2258a7e002107a89616494cfcab246
dansandu <daniel.sandu7@gmail.com>
2018-10-08 17:22:29 +0300

    Refactor line tracer

--- source/dansandu/eyecandy/raster/line_tracer.hpp
+++ source/dansandu/eyecandy/raster/line_tracer.hpp
@@ -1,4 +1,6 @@
-#include "dansandu/eyecandy/math/point.hpp"
+#pragma once
+
+#include "dansandu/eyecandy/math/matrix.hpp"
 
 namespace dansandu {
 namespace eyecandy {
@@ -6,17 +8,18 @@ namespace raster {
 
 class LineTracer {
 public:
-    using Point = dansandu::eyecandy::math::Point;
+    using size_type = dansandu::eyecandy::math::size_type;
+    using Point = dansandu::eyecandy::math::Matrix<size_type, 1, 2>;
 
-    LineTracer(Point a, Point b)
-        : width_{std::abs(b.x - a.x)},
-          height_{std::abs(b.y - a.y)},
-          dx_{a.x < b.x ? 1 : -1},
-          dy_{a.y < b.y ? 1 : -1},
+    LineTracer(const Point& a, const Point& b)
+        : width_{std::abs(b.x() - a.x())},
+          height_{std::abs(b.y() - a.y())},
+          dx_{a.x() < b.x() ? 1 : -1},
+          dy_{a.y() < b.y() ? 1 : -1},
           error_{height_ <= width_ ? 2 * height_ - width_ : height_ - 2 * width_},
-          end_{height_ <= width_ ? b.x : b.y},
-          x_{a.x},
-          y_{a.y} {}
+          end_{height_ <= width_ ? b.x() : b.y()},
+          x_{a.x()},
+          y_{a.y()} {}
 
     auto step() {
         if (height_ <= width_ && x_ != end_) {
@@ -39,17 +42,17 @@ public:
         return false;
     }
 
-    Point position() const { return {x_, y_}; }
+    auto position() const { return Point{{x_, y_}}; }
 
 private:
-    int width_;
-    int height_;
-    int dx_;
-    int dy_;
-    int error_;
-    int end_;
-    int x_;
-    int y_;
+    size_type width_;
+    size_type height_;
+    size_type dx_;
+    size_type dy_;
+    size_type error_;
+    size_type end_;
+    size_type x_;
+    size_type y_;
 };
 }
 }
