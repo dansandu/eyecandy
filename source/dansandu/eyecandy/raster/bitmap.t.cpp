cc26a7a3a764ef179226f4f3714ef55964a04ec3
dansandu <daniel.sandu7@gmail.com>
2018-10-08 16:21:08 +0300

    Reduce bitmap baseline test file size

--- source/dansandu/eyecandy/raster/bitmap.t.cpp
+++ source/dansandu/eyecandy/raster/bitmap.t.cpp
@@ -8,13 +8,14 @@ using dansandu::eyecandy::raster::writeBitmapFile;
 
 TEST_CASE("Bitmap") {
     SECTION("chess board") {
-        constexpr auto squareSize = 20;
-        Image chessboard{squareSize * 8, squareSize * 8};
+        constexpr auto numberOfSquares = 4;
+        constexpr auto squareSize = 10;
+        Image chessboard{squareSize * numberOfSquares, squareSize * numberOfSquares};
         for (auto x = 0; x < chessboard.width(); ++x)
             for (auto y = 0; y < chessboard.height(); ++y)
                 chessboard.plot(x, y, (x / squareSize + y / squareSize) % 2 ? Colors::white : Colors::turquoise);
 
-        constexpr auto bitmapFileName = "resource/chessboard.bmp";
+        constexpr auto bitmapFileName = "resource/expected_chessboard.bmp";
         writeBitmapFile(bitmapFileName, chessboard);
         Image readChessboard = readBitmapFile(bitmapFileName);
 
